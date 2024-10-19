427d2ba5ef2f7865f523c9897d6172207c233b12
hazelcaling <hazelcaling@live.com>
2024-10-19 10:54:49 -0700

    Update uncompress with improved documentation

--- arrays-and-strings/uncompress.js
+++ arrays-and-strings/uncompress.js
@@ -1,9 +1,28 @@
 /*
-Write a function, uncompress, that takes in a string as an argument.
-The input string will be formatted into multiple groups according to the following pattern:
-<number><char>
-for example: '2c', '33a'
-The function should return an uncompressed version of the string where each char of a group is repeated number times consecutively. You may assume that the input string is well-formed according to the previously metioned pattern.
+Problem:
+    Write a function, uncompress, that takes in a string as an argument.
+    The input string will be formatted into multiple groups according to the following pattern:
+    <number><char>, for example: '2c', '33a'.
+    The function should return an uncompressed version of the string where each char of a group 
+    is repeated number times consecutively. You may assume that the input string is well-formed 
+    according to the previously mentioned pattern.
+
+Approach:
+    - Initialize two pointers, i and j, to track the current indices in the string.
+    - Use an array to collect the result.
+    - Iterate through the string using j to identify the current character:
+        - If the current character is a digit, continue incrementing j.
+        - Once a character (not a digit) is found, slice the string from i to j to get the number,
+          repeat the character by that number, and push the result into the array.
+        - Update i to the current position of j.
+    - Join the array into a string and return it.
+
+Complexity:
+    n - length of the input string
+    Time - O(n)
+        - We traverse the string a single time to construct the result.
+    Space - O(n)
+        - The space used is proportional to the size of the output string.
 */
 
 const uncompress = (str) => {
@@ -16,10 +35,9 @@ const uncompress = (str) => {
         const currChar = str[j];
         if (numbers.includes(currChar)) {
             j += 1;
-
         } else {
             const num = str.slice(i, j)
-            res.push(str[j].repeat(num));
+            res.push(currChar.repeat(num));
             j += 1;
             i = j;
         }
@@ -29,14 +47,6 @@ const uncompress = (str) => {
 
 }
 
-
-
-/*
-    j
-3a10b
-  i
-
-*/
-
-console.log(uncompress('2c3a1t'))
-console.log(uncompress('5a'))
+// Test cases
+console.log(uncompress('2c3a1t')) // -> 'ccaaat'
+console.log(uncompress('5a')) // -> 'aaaaa'
